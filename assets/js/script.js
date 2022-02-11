3c0e74739c39ffa0d991aa4dcd1e30da8ece9f32
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-02-10 20:36:41 -0500

    fixed html & css of table inputs, added js click for another row in table & started local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,83 @@
-$("#sendGoogle").on('click', function(event) {
-    event.preventDefault;
-    var searchInput = $("#inputGoogle").val();
-    console.log(searchInput);
+//ADD ROW TO TABLE FUNCTIONS
+$(".add_another").click(function (event) {
+  event.preventDefault;
+  $("#table-body").append(
+    `<tr>
+    <td class="pt-3-half" contenteditable="false"><input type="checkbox" class="btn-check toggleButton" id="toggleButton1"></td>
+    <td class="pt-3-half" contenteditable="true">Sample Text Here</td>
+    <td class="pt-3-half" contenteditable="true">Sample</td>
+    </tr>`
+  );
+});
 
-    fetch("https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyDgLwR0nvp94v9yFrzBQOTbaERziTLQ9mM&cx=7725374fca0735be0&q="+searchInput)
-    .then(response => {
-        // error checking the response
-        if (!response.ok) {
-            throw response;
-        }
-        // then returning the response in .json() format, which makes it an object
-        return response.json();
+//LOCAL STORAGE FUNCTIONS ON SAVE BUTTON
+//1. TABLE BODY INPUT
+$(".save-btn").on("click", function () {
+  var inputId = $(this).find(".input").attr("id");
+  var inputValue = $("#" + inputId).val();
+
+  localStorage.setItem(inputId, inputValue);
+});
+
+//PREVENT INPUT CLEAR ON PAGE REFRESH
+function showInput() {
+  var tableEl = $(".input");
+  tableEl.each(function (i) {
+    console.log(i);
+    var inputKey = $(this).attr("id");
+    var getInput = localStorage.getItem(inputKey);
+    $(this).val(getInput);
+  });
+}
+showInput();
+
+//2. NEW ROW
+
+
+
+
+
+
+
+//GOGGLE API
+$("#sendGoogle").on("click", function (event) {
+  event.preventDefault;
+  var searchInput = $("#inputGoogle").val();
+  console.log(searchInput);
+
+  fetch(
+    "https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyDgLwR0nvp94v9yFrzBQOTbaERziTLQ9mM&cx=7725374fca0735be0&q=" +
+      searchInput
+  )
+    .then((response) => {
+      // error checking the response
+      if (!response.ok) {
+        throw response;
+      }
+      // then returning the response in .json() format, which makes it an object
+      return response.json();
     })
 
-    .then(data => {
-        // this console.log is to check the data and read it all to find out what I want to add to the page
-        console.log(data);
+    .then((data) => {
+      // this console.log is to check the data and read it all to find out what I want to add to the page
+      console.log(data);
 
-        // need to empty the past search results if there are any
-        $("#googleResult").empty();
+      // need to empty the past search results if there are any
+      $("#googleResult").empty();
 
-        // add the result count and time
-        $("#googleResult").append(function() {
-            return `<div class="row text-grey" id="googleInfo">About ${data.searchInformation.formattedTotalResults} results (${data.searchInformation.formattedSearchTime} seconds)</div>`
-        });
+      // add the result count and time
+      $("#googleResult").append(function () {
+        return `<div class="row text-grey" id="googleInfo">About ${data.searchInformation.formattedTotalResults} results (${data.searchInformation.formattedSearchTime} seconds)</div>`;
+      });
 
-        // looping over the first 10 results from the search
-        for (var i = 0; i < 10; i++) {
-            // creating an easier to use variable for the one result that we are looking at
-            var result = data.items[i];
+      // looping over the first 10 results from the search
+      for (var i = 0; i < 10; i++) {
+        // creating an easier to use variable for the one result that we are looking at
+        var result = data.items[i];
 
-            // this appends the results to that id
-            $("#googleResult").append(function() {
-                return `<div class="row result-box text-black bg-light" id="result${0}">
+        // this appends the results to that id
+        $("#googleResult").append(function () {
+          return `<div class="row result-box text-black bg-light" id="result${0}">
                     <h5 class="text-info small">
                         ${result.displayLink}
                     </h5>
@@ -42,53 +87,62 @@ $("#sendGoogle").on('click', function(event) {
                     <p>
                         ${result.htmlSnippet}
                     </p>
-                </div>`
-            });
-        }
+                </div>`;
+        });
+      }
     })
     .catch(console.error);
 });
 
-$('#sendYoutube').on('click', function (event) {
-    event.preventDefault;
-    var searchInput = $("#inputYoutube").val();
+//YOUTUBE API
+$("#sendYoutube").on("click", function (event) {
+  event.preventDefault;
+  var searchInput = $("#inputYoutube").val();
 
-    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="+searchInput+"&key=AIzaSyCLYNdBi4AZz2fvgLA6cw8cEhl88E-oCps")
-    .then(response => {
-        if (!response.ok) {
-            throw response;
-        }
-        return response.json();
+  fetch(
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" +
+      searchInput +
+      "&key=AIzaSyCLYNdBi4AZz2fvgLA6cw8cEhl88E-oCps"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
     })
 
-    .then(data => {
-        console.log(data);
+    .then((data) => {
+      console.log(data);
 
-        // need to empty the past search results if there are any
-        $("#youtubeResult").empty();
+      // need to empty the past search results if there are any
+      $("#youtubeResult").empty();
 
-        // add the result count and time
-        $("#youtubeResult").append(function() {
-            return `<div class="row text-grey" id="youtubeInfo">Showing Top 5 Relevant Results</div>`
-        });
+      // add the result count and time
+      $("#youtubeResult").append(function () {
+        return `<div class="row text-grey" id="youtubeInfo">Showing Top 5 Relevant Results</div>`;
+      });
 
-        // looping over the first 10 results from the search
-        for (var i = 0; i < 5; i++) {
-            // creating an easier to use variable for the one result that we are looking at
-            var result = data.items[i];
+      // looping over the first 10 results from the search
+      for (var i = 0; i < 5; i++) {
+        // creating an easier to use variable for the one result that we are looking at
+        var result = data.items[i];
 
-            // this appends the results to that id
-            $("#searchCard").append(function() {
-                return `<div class="row result-box text-black bg-light" id="result${0}">
+        // this appends the results to that id
+        $("#searchCard").append(function () {
+          return `<div class="row result-box text-black bg-light" id="result${0}">
                     <h5 class="small">
-                        <a href="https://www.youtube.com/watch?v=${result.id.videoId}">${result.snippet.title}</a>
+                        <a href="https://www.youtube.com/watch?v=${
+                          result.id.videoId
+                        }">${result.snippet.title}</a>
                     </h5>
                     <h4>
-                        <img src="${result.snippet.thumbnails.default.url}" alt=${result.id.kind}>
+                        <img src="${
+                          result.snippet.thumbnails.default.url
+                        }" alt=${result.id.kind}>
                     </h4>
-                </div>`
-            });
-        }
+                </div>`;
+        });
+      }
     })
     .catch(console.error);
 });
